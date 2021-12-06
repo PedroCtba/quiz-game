44737b1803ffed029d287f96cd5a95ac41160ac3
PedroCtba <miyasakigazola@gmail.com>
2021-12-06 18:10:52 -0300

    names of .pys fixed

---
+++ question_model.py
@@ -0,0 +1,7 @@
+# create class Question with 2 inital atrobutes defined by the program
+class Question:
+    def __init__(self, text, answer):
+
+        # the atributes are "text" and "answer"
+        self.text = text
+        self.answer = answer
