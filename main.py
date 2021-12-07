fbd705f97ad75d94871265aae1d58979d05090d7
PedroCtba <miyasakigazola@gmail.com>
2021-12-07 08:28:19 -0300

    finalizado

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # importing data with qiestios and object to construct questions
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
+# making a question bank
+question_bank = [(Question(dic['text'], dic['answer'])) for dic in question_data]
 
-questions = [(Question(dic['text'], dic['answer'])) for dic in question_data]
+# init quiz brain
+quiz = QuizBrain(question_bank)
+quiz.ask()
---
+++ quiz_brain.py
@@ -0,0 +1,31 @@
+# make class quiz brain with 2 atributes, the number of the question, and the list of questions
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_number = 0 # this always initialize as 0
+        self.score = 0 # this always initialize as 0
+        self.question_list = question_list
+        
+
+    def ask(self):
+        current_q = self.question_number
+        self.question_number += 1
+        
+        reply = input(f'{self.question_list[current_q].text} (True/False): ')
+        self.check(reply, self.question_list[current_q].answer)
+        
+
+    def keep_asking(self):
+        if self.question_number <= len(self.question_list):
+            return True
+        else:
+            return False
+
+
+    def check(self, reply, current_a):
+        if reply.lower() == current_a.lower():
+            self.score += 1
+            print(f'You are right! Your current score is {self.score}/{self.question_number - 1}')
+            return True
+        else:
+            print(f'Sorry that"s wrong :( Your current score is {self.score}/{self.question_number - 1}')
+            return False
